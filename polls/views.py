9adebddbc4f27b419713a4cf87d7c2b820989d8d
ChristopherChantres <chris.chantres@gmail.com>
2023-04-09 09:49:41 -0400

    Views and Url added to /create and /edit. Logic changed in [block main]

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+
+def create(request):
+  return render(request, 'create.html')
+
+
+def edit(request):
+  return render(request, 'edit.html')
