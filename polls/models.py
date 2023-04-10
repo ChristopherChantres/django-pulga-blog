6e134e8f079545619261f05605cea201a5bd3da9
ChristopherChantres <chris.chantres@gmail.com>
2023-04-10 10:06:25 -0400

    First post created successfully

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=255)
+  author = models.CharField(max_length=30, default="Anonymous")
   content = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now_add=True)
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def home(request):
@@ -6,7 +9,20 @@ def home(request):
 
 
 def create(request):
-  return render(request, 'create.html')
+  if request.method == 'POST':
+    post_title = request.POST['post_title']
+    post_author = request.POST['post_author']
+    post_content = request.POST['post_content']
+
+    if Post.objects.filter(title=post_title).exists():
+      messages.info(request, 'Post title alredy used')
+      return redirect('create')
+    else:
+      new_post = Post.objects.create(title=post_title, author=post_author, content=post_content, created_at=timezone.now(), updated_at=timezone.now())
+      new_post.save()
+      return redirect('home')
+  else:
+    return render(request, 'create.html')
 
 
 def edit(request):
