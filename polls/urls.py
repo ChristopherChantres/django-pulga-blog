7f88e377a37ac53b0f6c9c5e12f02561955424dc
ChristopherChantres <chris.chantres@gmail.com>
2023-04-11 10:35:08 -0400

    Posts links in the home page work properly

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('create', views.create, name='create'),
-  path('edit', views.edit, name='edit')
+  path('edit', views.edit, name='edit'),
+  path('post/<int:post_id>', views.post_detail, name='post_detail'),
 ]
