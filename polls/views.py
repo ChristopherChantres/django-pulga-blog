062c6e82497e0d796124f20d991a3aac7101021d
ChristopherChantres <chris.chantres@gmail.com>
2023-04-22 18:40:57 -0400

    Posts sorted by newest to oldest

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,7 @@ from .models import Post
 
 # Create your views here.
 def home(request):
-  posts = Post.objects.all()
+  posts = Post.objects.all().order_by('-created_at')
   return render(request, 'home.html', {'posts':posts})
 
 
