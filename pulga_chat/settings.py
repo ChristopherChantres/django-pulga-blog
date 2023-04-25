3782b3b3432fe184598f835591d0ea53c9ccf4c1
ChristopherChantres <chris.chantres@gmail.com>
2023-04-25 16:41:27 -0400

    Pycache thing

--- pulga_chat/settings.py
+++ pulga_chat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-dzgmkzp(stq)*&a(jqj&twtbcrq4&lwz1v&4k)@3$a%+a!v#68
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '10.0.0.85']
+ALLOWED_HOSTS = ['127.0.0.1', '10.135.163.10']
 
 
 # Application definition
